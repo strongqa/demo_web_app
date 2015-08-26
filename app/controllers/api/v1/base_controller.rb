98c498c24b9beae95a3b65c6f0385ae2f8bf450a
Roman Parashchenko <romikoops1@gmail.com>
2015-08-26 21:34:35 +0200

    Fixed problem

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,7 +1,7 @@
 require "application_responder"
 
 class API::V1::BaseController < ApplicationController
-  skip_before_action :verify_authenticity_token
+  protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
 
   self.responder = ApplicationResponder
 
