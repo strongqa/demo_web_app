002feea30b8cfe6b195e3f03636ebfcb9d5f2fcd
Roman Parashchenko <romikoops1@gmail.com>
2016-06-07 18:19:32 +0200

    fixed db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,9 +4,8 @@ Article.destroy_all
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
 
-50.times { CreateDemoUserService.new.call }
-puts 'Created 50 test users'
+5.times { CreateDemoUserService.new.call }
+puts 'Created 5 test users'
 
-30.times { CreateDemoArticleService.new.call }
-puts 'Created 30 test aticles'
-
+3.times { CreateDemoArticleService.new.call }
+puts 'Created 3 test aticles'
