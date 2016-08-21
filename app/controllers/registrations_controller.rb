7230b62912ad16a98c0879b074112a63bab3c336
Roman Parashchenko <romikoops1@gmail.com>
2016-08-21 16:37:44 +0200

    migrated devise method

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,9 +2,9 @@ class RegistrationsController < Devise::RegistrationsController
   before_action :update_sanitized_params, if: :devise_controller?
 
   def update_sanitized_params
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
-    devise_parameter_sanitizer.for(:account_update) do |u|
-      u.permit(:name, :email, :password, :password_confirmation, :current_password)
-    end
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation])
+    devise_parameter_sanitizer.permit(
+      :account_update, keys: [:name, :email, :password, :password_confirmation, :current_password]
+    )
   end
 end
