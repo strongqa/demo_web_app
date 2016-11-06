0e2c3b9b8573999c3f7b42ed86b2b72ae5e69a40
Roman Parashchenko <romikoops1@gmail.com>
2016-11-06 17:38:28 +0100

    fix devise migration

--- app/services/create_demo_user_service.rb
+++ app/services/create_demo_user_service.rb
@@ -3,7 +3,7 @@ class CreateDemoUserService
     User.find_or_create_by!(email: Faker::Internet.free_email, name: Faker::Internet.user_name) do |user|
       user.password = 'Password1234'
       user.password_confirmation = 'Password1234'
-      user.confirm!
+      user.confirm
     end
   end
 end
