48dc0b71303fa9acff188c6911d40381ba5f8278
IvanZymohliad <izymohliad@strongqa.com>
2018-03-14 17:18:49 +0200

    style homepage, add dynamic dates on homepage and articles

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -9,4 +9,25 @@ module ArticlesHelper
   def today_or_date(date)
     date == Time.zone.today ? 'Today' : date
   end
+
+  def show_day(created_at)
+    created_at.strftime('%d')
+  end
+
+  def show_month_short(created_at)
+    created_at.strftime('%b')
+  end
+
+  def show_date(created_at)
+    created_at.strftime('%d %B, %Y')
+  end
+
+  def count_comments(comments)
+    if comments.any?
+      comments_count = comments.count
+      comments_count > 1 ? "#{comments_count} Comments" : '1 Comment'
+    else
+      'No comments yet'
+    end
+  end
 end
