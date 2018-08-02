887da946b3cacbcda21fd6786f4bd177c5521914
greenhairedlime <kalinichenko1masha@gmail.com>
2018-08-02 19:11:04 +0300

    updated users api spec

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Users', type: :request do # rubocop:disable Metrics/BlockLength
+RSpec.describe 'UsersController', type: :request do # rubocop:disable Metrics/BlockLength
   headers = { 'Authorization': "Token token=#{ENV['HOWITZER_TOKEN']}" }
 
   describe 'GET #index' do
@@ -18,7 +18,7 @@ RSpec.describe 'Users', type: :request do # rubocop:disable Metrics/BlockLength
 
   describe 'GET #show' do
     let!(:users) { create_list(:user, 5) }
-    context 'existing user'do
+    context 'existing user' do
       before { get '/api/v1/users/3', headers: headers }
 
       it 'returns a successful response' do
@@ -30,99 +30,110 @@ RSpec.describe 'Users', type: :request do # rubocop:disable Metrics/BlockLength
       end
     end
 
-    context 'not existing user'do
+    context 'not existing user' do
       it 'raises an error' do
         assert_raises(ActiveRecord::RecordNotFound) do
-          get '/api/v1/users/1234',  headers: headers
+          get '/api/v1/users/1234', headers: headers
         end
       end
     end
   end
 
-#   describe 'GET #create' do
-#     it 'returns a successful response' do
-#       get '/api/v1/users', params: create(:user, name: 'Jon,'), headers: headers
-#       p json
-#       p response
-#       expect(response).to have_http_status(:created)
-#     end
-#   end
-#
-#   describe 'GET #edit' do
-#     it 'returns a successful response' do
-#       get edit_user_path create(:user)
-#       expect(response).to be_successful
-#     end
-#   end
-#
-#   describe 'POST #create' do
-#     context 'with valid attributes' do
-#       it 'creates the record in the database' do
-#         expect do
-#           post users_path, params: { user: attributes_for(:user) }
-#         end.to change(User, :count).by(1)
-#       end
-#
-#       it 'redirects to users#show' do
-#         post users_path, params: { user: attributes_for(:user) }
-#         expect(response).to redirect_to user_url(User.first)
-#       end
-#     end
-#
-#     context 'with invalid attributes' do
-#       it 'does not create a record in the database' do
-#         expect do
-#           post users_path, params: { user: attributes_for(:user, :invalid) }
-#         end.to_not change(User, :count)
-#       end
-#
-#       it 'returns a successful response' do
-#         post users_path, params: { user: attributes_for(:user, :invalid) }
-#         expect(response).to be_successful
-#       end
-#     end
-#   end
-#
-#   describe 'PATCH #update' do
-#     let!(:user) { create(:user, email: 'john@example.com') }
-#
-#     context 'with valid attributes' do
-#       it 'updates the record in the database' do
-#         patch user_path user, params: { user: attributes_for(:user, email: 'jane@example.com') }
-#         expect(user.reload.email).to eq 'jane@example.com'
-#       end
-#
-#       it 'redirects to users#show' do
-#         patch user_path user, params: { user: attributes_for(:user) }
-#         expect(response).to redirect_to user
-#       end
-#     end
-#
-#     context 'with invalid attributes' do
-#       it 'does not update the record in the database' do
-#         patch user_path user, params: { user: attributes_for(:user, :invalid) }
-#         expect(user.reload.email).to eq 'john@example.com'
-#       end
-#
-#       it 'returns a successful response' do
-#         patch user_path user, params: { user: attributes_for(:user, :invalid) }
-#         expect(response).to be_successful
-#       end
-#     end
-#   end
-#
-#   describe 'DELETE #destroy' do
-#     let(:user) { build(:user) }
-#
-#     it 'deletes the record from the database' do
-#       expect do
-#         delete user_path user
-#       end.to change(User, :count).by(-1)
-#     end
-#
-#     it 'redirects to users#index' do
-#       delete user_path :user
-#       expect(response).to redirect_to users_url
-#     end
-#   end
+  describe 'GET #create' do
+    it 'returns a successful response' do
+      get api_v1_users_path,
+          params: create(:user),
+          headers: headers
+      expect(response).to be_successful
+    end
+  end
+
+  describe 'GET #update' do
+    it 'returns a successful response' do
+      get "/api/v1/users/#{create(:user).id}", headers: headers
+      expect(response).to be_successful
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'creates the record in the database' do
+        expect do
+          post api_v1_users_path, params: { user: attributes_for(:user) }, headers: headers
+        end.to change(User, :count).by(1)
+      end
+
+      it 'returns a created status' do
+        post api_v1_users_path, params: { user: attributes_for(:user) }, headers: headers
+        expect(response).to have_http_status(:created)
+      end
+    end
+
+    context 'with invalid attributes' do
+      before do
+        post api_v1_users_path,
+             params: { user: { email: '', name: 'Test', password: '112', is_admin: false } },
+             headers: headers
+      end
+
+      it 'provide errors' do
+        expect(json['errors']).to eq('email' => ["can't be blank"],
+                                     'password' => ['is too short (minimum is 8 characters)'])
+      end
+
+      it 'returns an unprocessable entity status' do
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
+
+  describe 'PATCH #update' do
+    let!(:user){ create(:user) }
+
+    # before {  }
+    context 'with valid attributes' do
+      it 'updates the record in the database' do
+        patch "/api/v1/users/#{user.id}", params: { user: attributes_for(:user, email: 'jane@example.com') }, headers: headers
+
+        p response.message
+        expect(user.reload.email).to eq 'jane@example.com'
+        # expect(User.find(user.id)[:email]).to eq 'jane@example.com'
+      end
+
+      it 'returns a successful response' do
+        patch "/api/v1/users/#{user.id}", params: { user: { id: user.id, email: 'jane@example.com'} }, headers: headers
+        expect(response).to be_successful
+        p response.message
+      end
+    end
+
+    # context 'with invalid attributes' do
+    #   it 'does not update the record in the database' do
+    #     patch user_path user, params: { user: attributes_for(:user, :invalid) }
+    #     expect(user.reload.email).to eq 'john@example.com'
+    #   end
+    #
+    #   it 'returns a successful response' do
+    #     patch user_path user, params: { user: attributes_for(:user, :invalid) }
+    #     expect(response).to be_successful
+    #   end
+    # end
+  end
+
+  describe 'DELETE #destroy' do
+    let!(:user) { create(:user) }
+
+    it 'deletes the record from the database' do
+      expect do
+        delete "/api/v1/users/#{user.id}",
+               headers: headers
+      end.to change(User, :count).by(-1)
+    end
+
+    it 'returns a no content response' do
+      delete "/api/v1/users/#{user.id}",
+             headers: headers
+      expect(response).to have_http_status(:no_content)
+    end
+  end
 end
