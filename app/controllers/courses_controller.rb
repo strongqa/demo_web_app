18e2a83e3a35aef043f60165c8260dadca2df932
ihorklymenko <klimklim136@gmail.com>
2021-06-02 10:48:13 +0300

    fix rubocop

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,9 +1,8 @@
 class CoursesController < ApplicationController
-  before_action :signed_in_as_admin?, except: %i[show index]
+  before_action :signed_in_as_admin?, except: %i[index]
   layout 'courses'
-  skip_before_action :require_login, only: %i[index show]
-  skip_before_action :require_admin, only: %i[index show]
+  skip_before_action :require_login, only: %i[index]
+  skip_before_action :require_admin, only: %i[index]
 
-  def index # rubocop:disable Metrics/AbcSize
-  end
+  def index; end
 end
