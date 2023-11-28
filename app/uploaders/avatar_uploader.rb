90c1b58f28779647ebb5f06258d55cd80d0696a3
Mirk32 <michael.matrosov@gmail.com>
2023-11-28 15:51:29 +0200

    Rubocop fix

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,5 +1,5 @@
 class AvatarUploader < CarrierWave::Uploader::Base
-    def store_dir
+  def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 end
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -11,14 +11,14 @@ else
   CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
-      provider:              'AWS',
-      aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region:                ENV['AWS_REGION']
+      provider: 'AWS',
+      aws_access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID', nil),
+      aws_secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY', nil),
+      region: ENV.fetch('AWS_REGION', nil)
     }
 
     config.storage = :fog
-    config.fog_directory  = ENV['S3_BUCKET']
+    config.fog_directory  = ENV.fetch('S3_BUCKET', nil)
     config.fog_public     = true
     config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
   end
